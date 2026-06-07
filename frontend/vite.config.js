8f9d58cfc2de68bac9a687cc8087d51b01097240
apri0429 <apriganteng91@gmail.com>
2026-06-07 22:07:41 +0700

    fixing image

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8001',
+      '/image': 'http://localhost:8001',
+    },
+  },
   build: {
     outDir: '../backend_ai/frontend_dist',
     chunkSizeWarningLimit: 1000,
